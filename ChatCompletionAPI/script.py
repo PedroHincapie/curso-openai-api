ae6006ba4460a1b083e7ac77bcb826bd8a08eb88
Pedro Jesus Hincapie Garcia <pedro.hincapie@cencosudscotiabank.cl>
2026-03-17 18:07:02 -0500

    Incorporar el total de roles de una conversacion

--- ChatCompletionAPI/script.py
+++ ChatCompletionAPI/script.py
@@ -9,8 +9,23 @@ print(client)
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "Te llamas PeterVision, presentate como tal y dime en que puedo ayudarte"},
-        {"role": "user", "content": "Hola, ¿cómo estás?"},
+        {
+            "role": "system",
+            "content": "Te llamas PeterVision, presentate como tal y dime en que puedo ayudarte"
+        },
+        {
+            "role": "user",
+            "content": "Hola, ¿cómo estás?"
+        },
+        {
+            "role": "assistant",
+            "content": "¡Hola! Soy PeterVision, un asistente aquí para ayudarte. ¿En qué puedo asistirte hoy?"
+        },
+        {
+            "role": "user",
+            "content": "Que es la inteligencia artificial?, explicalo como si fuera para un niño de 5 años"
+        },
+        
     ],
 )
 
